6a9ad218b5b4b8d9961df1b0a5f94f98507acecb
linglian <linglian@aliyun.com>
2016-10-13 20:39:16 +0800

    待解决旋转问题

--- Assets/Script/DrawMap.cs
+++ Assets/Script/DrawMap.cs
@@ -3,25 +3,33 @@ using System.Collections;
 
 public class DrawMap : MonoBehaviour {
 	public int [][]map;
+	public GameObject wall;
 	// Use this for initialization
 	void Start () {
-		map = new int[50][50];
+		map = new int[50][];
 		for (int i = 0; i < 50; i++) {
+			map[i] = new int[50];
 			for (int j = 0; j < 50; j++) {
 				if (i == 0 || j == 0 || i == 49 || j == 49) {
 					map [i] [j] = 1;
 				}
 			}
 		}
+		drawMap ();
 	}
 
 	void drawMap(){
+		Vector3 pos;
+		pos.y = 2.6f;
 		for (int i = 0; i < 50; i++) {
 			for (int j = 0; j < 50; j++) {
+				pos.x = (float)i-26.5f;
+				pos.z = (float)j-26.5f;
 				switch(map[i][j]){
 				case 0:
 					break;
 				case 1:
+					Instantiate (wall, pos,Quaternion.identity);
 					break;
 				}
 			}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public GameObject snake;
+	public float speed;
+	public float rSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float t = Input.GetAxis ("Vertical") * speed*Time.deltaTime;
+		float r = Input.GetAxis ("Horizontal") * rSpeed * Time.deltaTime;
+		snake.transform.Translate(0, 0, t);
+		snake.transform.Rotate (0, r, 0);
+	}
+}
